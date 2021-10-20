e78e5ed8b8625150b92c6a139ac8a06dcaaaa372
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-10-21 02:44:16 +0300

    refresh class name of empty cell

--- check_free_place.rb
+++ check_free_place.rb
@@ -9,7 +9,7 @@ Minsk_Passangers = '%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%9F%D0%B0%D1%81%D1%81%D0%B
 loop do
   rw_by_link = "https://pass.rw.by/ru/route/?from=#{Pinsk}&from_esr=&from_exp=&to=#{Minsk_Passangers}&to_esr=&to_exp=&date=#{ARGV[0]}"
   rw_by_page = Nokogiri::HTML(Curl.get(rw_by_link).body_str)
-  cell_with_no_seats = rw_by_page.xpath('//div[starts-with(@class, "sch-table__no-info")]')
+  cell_with_no_seats = rw_by_page.xpath('//div[starts-with(@class, "sch-table__cell cell-4 empty")]')
   print `say "I found a ticket"` if cell_with_no_seats.empty?
   print '.'.green
   sleep(1)
