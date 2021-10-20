efb7ec55b6d66c145a80a406eb1f6ec49608d3a6
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-10-21 01:11:02 +0300

    refine while

--- check_free_place.rb
+++ check_free_place.rb
@@ -6,7 +6,7 @@ require 'colorize'
 Pinsk = '%D0%9F%D0%B8%D0%BD%D1%81%D0%BA'
 Minsk_Passangers = '%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%9F%D0%B0%D1%81%D1%81%D0%B0%D0%B6%D0%B8%D1%80%D1%81%D0%BA%D0%B8%D0%B9'
 
-while 1 do
+loop do
   rw_by_link = "https://pass.rw.by/ru/route/?from=#{Pinsk}&from_esr=&from_exp=&to=#{Minsk_Passangers}&to_esr=&to_exp=&date=#{ARGV[0]}"
   rw_by_page = Nokogiri::HTML(Curl.get(rw_by_link).body_str)
   cell_with_no_seats = rw_by_page.xpath('//div[starts-with(@class, "sch-table__no-info")]')
